d18f933b6329ceb7291b4731254b8e4f381074e6
Santosh singh <60971475+santosh214@users.noreply.github.com>
2023-09-29 23:05:10 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '@/Layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
+  console.log("hello")
   return (
     <Layout>
       <Component {...pageProps} />
